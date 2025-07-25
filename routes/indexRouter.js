fb267794305618c33799a1879b15510b18d5e3d2
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-25 12:16:02 -0400

    Add messages to index route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,22 @@
 // routes/indexRouter.js
 const { Router } = require("express");
-
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
 indexRouter.get("/", (req, res) => {
-  res.render("index", { message : "This is an index page!" });
+    res.render("index", { messages : messages });
 });
 
 module.exports = indexRouter;
